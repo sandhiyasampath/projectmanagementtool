9cdfe3553adf7b3ee5daffdf96c4176f02ddd6d4
sandhiyasampath <anbu171298@gmail.com>
2020-12-04 16:43:56 +0530

    converded DOB to age

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from.models import Employees,Projects
 from datetime import date 
 
+
 # Create your views here.
 def home(request):
         details = Employees.objects.all()
@@ -9,7 +10,10 @@ def home(request):
 
 def employeeDetails(request,id):
         detail = Employees.objects.get(Employee_ID=id)
-        return render(request,'empDetails.html',{'detail' : detail})
+        today = date.today() 
+        age = (today.year - detail.Date_Of_Birth.year - ((today.month, today.day) < (detail.Date_Of_Birth.month, detail.Date_Of_Birth.day)))
+
+        return render(request,'empDetails.html',{'detail' : detail,'age':age})
 
 
   
